565feea8a0bcd2c9a4c984e91375fbc8a959277b
Barney Randall <sevendeadlycommits@protonmail.com>
2020-12-26 13:42:16 +0000

    add unverified data download to allow different coins to be analysed

--- analyser.py
+++ analyser.py
@@ -1,29 +1,44 @@
 from matplotlib import pyplot as plt
+import argparse
 
 import coin_data
 import simple_result_data
 import processor
 import data_importer
 
-def main():
+running = True
+
+def ProcessArgs():
+    parser = argparse.ArgumentParser(description='Analyse historic crytpo data')
+    parser.add_argument('coin', metavar='-c', type=str,
+                        help='The cryptocurrency to analyse')
+
+    return parser.parse_args()
+
+def Run():
+    args = ProcessArgs()
+
     importer = data_importer.DataImporter()
-    data_frame = importer.ImportToDataFrameFromCSV('data/Bittrex_ETHUSD_1h.csv')
+    data_frame = importer.GetData(args.coin)
     data_processor = processor.Processor()
     result = data_processor.ProcessSimple(data_frame)
 
     print('Simple Results:\n' + str(result))
+
+def main():
+    Run()
     
-    lows = data_processor.GetColumn(data_frame, 'Low')
-    highs = data_processor.GetColumn(data_frame, 'High')
+    # lows = data_processor.GetColumn(data_frame, 'Low')
+    # highs = data_processor.GetColumn(data_frame, 'High')
     
-    plt.title('Highs/Lows (all time)')
-    plt.plot(highs, label='Highs')
-    plt.plot(lows, label='Lows')
-    plt.ylabel('Coin Value')
-    plt.xlabel('Index')
-    plt.legend()
-    plt.savefig('output/ETHUSD_HI_LO.png')
-    plt.show()
+    # plt.title('Highs/Lows (all time)')
+    # plt.plot(highs, label='Highs')
+    # plt.plot(lows, label='Lows')
+    # plt.ylabel('Coin Value')
+    # plt.xlabel('Index')
+    # plt.legend()
+    # plt.savefig('output/ETHUSD_HI_LO.png')
+    # plt.show()
 
 if __name__ == "__main__":
     main()
--- data_importer.py
+++ data_importer.py
@@ -1,7 +1,23 @@
 import pandas
+import requests
 
 class DataImporter:
+    url = 'https://www.cryptodatadownload.com/cdd/'
+    file_prefix = 'Bittrex_'
+    end = 'USD_1h.csv'
 
-    @staticmethod
-    def ImportToDataFrameFromCSV(path):
+    def ImportToDataFrameFromCSV(self, path):
         return pandas.read_csv(path)
+
+    def GetData(self, coin):
+        filename = self.file_prefix + coin + self.end
+        endpoint = self.url + filename
+        data_file = requests.get(endpoint, 'data/' + filename, verify=False)
+        open('data/' + filename, 'wb').write(data_file.content)
+        
+        with open('data/' + filename, 'r') as fin:
+            data = fin.read().splitlines(True)
+        with open('data/' + filename, 'w') as fout:
+            fout.writelines(data[1:])
+        
+        return self.ImportToDataFrameFromCSV('data/' + filename)
