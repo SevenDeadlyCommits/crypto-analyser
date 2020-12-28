97abcea208663b931dd440dc8d550dbf5b978f31
Barney Randall <sevendeadlycommits@protonmail.com>
2020-12-28 00:03:28 +0000

    added beginnings of command options and caching

--- analyser.py
+++ analyser.py
@@ -7,12 +7,20 @@ import processor
 import data_importer
 import constants
 
-running = True
-
 def process_args():
     parser = argparse.ArgumentParser(description='Analyse historic crytpo data')
     parser.add_argument('coin', metavar='COIN', type=str, choices=constants.COIN_SELECTION,
                         help='The cryptocurrency to analyse')
+
+    parser.add_argument('-t', '--type', type=str, choices=constants.PLOT_TYPES,
+                        help='The plot type requested')
+    parser.add_argument('-s', '--show', required=False, action='store_true',
+                        help='Whether to show the plot once created ')
+    parser.add_argument('-n', '--no-save', required=False, action='store_true',
+                        help='Whether to save the plot to disk as a png')
+
+    parser.add_argument('-i', '--info-type', required=False, type=str, choices=constants.INFO_TYPES,
+                        help='The type of information to get out of the analyser')
 
     return parser.parse_args()
 
@@ -22,24 +30,28 @@ def run():
     importer = data_importer.DataImporter()
     data_frame = importer.get_data(args.coin)
     data_processor = processor.Processor()
-    result = data_processor.process_simple(data_frame)
 
-    print('Simple Results:\n' + str(result))
+    if args.type:
+        lows = data_processor.get_column(data_frame, 'Low')
+        highs = data_processor.get_column(data_frame, 'High')
+        
+        plt.title('Highs/Lows (all time)')
+        plt.plot(highs, label='Highs')
+        plt.plot(lows, label='Lows')
+        plt.ylabel('Coin Value')
+        plt.xlabel('Index')
+        plt.legend()
+        if args.no_save == False:
+            plt.savefig('output/ETHUSD_HI_LO.png')
+        if args.show:
+            plt.show()
+
+    if args.info_type:
+        result = data_processor.process_simple(data_frame)
+        print('Simple Results:\n' + str(result))
 
 def main():
     run()
     
-    # lows = data_processor.GetColumn(data_frame, 'Low')
-    # highs = data_processor.GetColumn(data_frame, 'High')
-    
-    # plt.title('Highs/Lows (all time)')
-    # plt.plot(highs, label='Highs')
-    # plt.plot(lows, label='Lows')
-    # plt.ylabel('Coin Value')
-    # plt.xlabel('Index')
-    # plt.legend()
-    # plt.savefig('output/ETHUSD_HI_LO.png')
-    # plt.show()
-
 if __name__ == "__main__":
     main()
--- data_importer.py
+++ data_importer.py
@@ -1,6 +1,7 @@
 import pandas
 import requests
 import os.path
+import time
 from os import path
 
 class DataImporter:
@@ -15,15 +16,31 @@ class DataImporter:
     def import_data_frame_from_csv(self, path):
         return pandas.read_csv(path)
 
+    def file_out_of_date(self, filename):
+        if os.path.isfile(filename):
+            # valid for 24 hrs
+            valid_time = time.time() - 86400
+            valid = (os.path.getmtime(filename) <= valid_time)
+            if valid:
+                valid = (os.path.getctime(filename) <= valid_time)
+            return valid
+        return False
+
     def get_data(self, coin):
         filename = self.file_prefix + coin + self.end
-        endpoint = self.url + filename
-        data_file = requests.get(endpoint, 'data/' + filename, verify=False)
-        open('data/' + filename, 'wb').write(data_file.content)
+        filename_with_path = 'data/' + filename
 
-        with open('data/' + filename, 'r') as fin:
-            data = fin.read().splitlines(True)
-        with open('data/' + filename, 'w') as fout:
-            fout.writelines(data[1:])
-        
-        return self.import_data_frame_from_csv('data/' + filename)
+        if self.file_out_of_date(filename_with_path):
+            print('Data out of date, requesting new dataset\n')
+            endpoint = self.url + filename
+            data_file = requests.get(endpoint, filename_with_path, verify=False)
+
+            # now process file to remove comment
+            open('data/' + filename, 'wb').write(data_file.content)
+
+            with open(filename_with_path, 'r') as fin:
+                data = fin.read().splitlines(True)
+            with open(filename_with_path, 'w') as fout:
+                fout.writelines(data[1:])
+
+        return self.import_data_frame_from_csv(filename_with_path)
